19a5d977d941d3a6c43904a59d66731a03469c14
Wouter Jan Klerk <wouterjan.klerk@deltares.nl>
2026-01-27 10:59:21 +0100

    fix test. Now uses the geojson from test_data

--- tests/test_dike_cost_integration.py
+++ tests/test_dike_cost_integration.py
@@ -1,72 +1,19 @@
 import pytest
 import geopandas as gpd
 from shapely.geometry import shape
+import numpy as np
 
 from app.dike_components.ground_model import GroundModel
+from app.dike_components.dike_model import DikeModel
 
 
 @pytest.fixture(scope="module")
 def dike_model():
-    geojson_input = {
-        "type": "FeatureCollection",
-        "crs": {"type": "name", "properties": {"name": "EPSG:4326"}},
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [[
-                        [5.568245382650625, 51.890881614488016, 8.8],
-                        [5.571640932264274, 51.891476523815435, 8.8],
-                        [5.57160887667709, 51.89154657512496, 8.9],
-                        [5.568213322196268, 51.890951664878685, 8.9],
-                        [5.568245382650625, 51.890881614488016, 8.8]
-                    ]]
-                },
-                "properties": {"name": "-34.9m_-26.8m"}
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [[
-                        [5.568213322196268, 51.890951664878685, 8.9],
-                        [5.57160887667709, 51.89154657512496, 8.9],
-                        [5.571578008239614, 51.891614031932704, 10.1],
-                        [5.56818244907187, 51.891019120801595, 10.1],
-                        [5.568213322196268, 51.890951664878685, 8.9]
-                    ]]
-                },
-                "properties": {"name": "-26.8m_-19m"}
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [[
-                        [5.56818244907187, 51.891019120801595, 10.1],
-                        [5.571578008239614, 51.891614031932704, 10.1],
-                        [5.571537245930476, 51.89170310949893, 14],
-                        [5.568141680573571, 51.891108197199316, 14],
-                        [5.56818244907187, 51.891019120801595, 10.1]
-                    ]]
-                },
-                "properties": {"name": "-19m_-8.7m"}
-            }
-        ]
-    }
-
-    features = []
-    for feature in geojson_input["features"]:
-        geom = shape(feature["geometry"])
-        features.append({"geometry": geom, **feature["properties"]})
-
-    gdf = gpd.GeoDataFrame(features, crs="EPSG:4326")
-    model = GroundModel(gdf)
-    model.calculate_volume()
+    gdf_ground = gpd.read_file('tests/test_data/test_berm__ontwerp_3d.geojson')
+    model = DikeModel(gdf_ground)
+    model.ground_model.calculate_volume()
 
     return model
-
 
 def test_3d_surface_area_positive(dike_model):
     area = dike_model.calculate_total_3d_surface_area()["total_3d_area_m2"]
@@ -92,10 +39,13 @@ def test_compute_cost_monotonic_road_area(dike_model):
 def test_groundwork_cost_nonzero(dike_model):
     costs = dike_model.compute_cost(nb_houses=0, road_area=0, complexity='makkelijke maatregel')
     EXPECTED_COST_DECOMPOSITION = {
-        'Directe kosten grondwerk': {'preparation_cost': 0, 'groundwork_cost': 284887.0606946243},
-        'Engineeringkosten': {'epk_cost': 22790.964855569946,
-                              'engineering_cost_schets': 16808.336580982836},
-        'Vastgoedkosten': {'Panden': 0, 'Wegen': 0.0}}
-    #NOTE: THis test is broken. should be checked and fixed later
-    assert costs["Directe kosten grondwerk"]["groundwork_cost"] == EXPECTED_COST_DECOMPOSITION['Directe kosten grondwerk']['groundwork_cost'] 
-    # assert costs == EXPECTED_COST_DECOMPOSITION
+        'Directe kosten grondwerk': {'preparation_cost': 914.04, 'groundwork_cost': 76489.88},
+        'Directe kosten constructies': {'directe_kosten_constructie': 0.0},
+        'Engineeringkosten': {'epk_cost': 8355.92,},
+        'Vastgoedkosten': {'house_cost': 0, 'road_cost': 0.0}}
+    np.testing.assert_allclose(costs["Directe kosten grondwerk"]["groundwork_cost"], EXPECTED_COST_DECOMPOSITION['Directe kosten grondwerk']['groundwork_cost'], rtol=1e-2)
+
+    #check if the full cost dict matches with floats within tolerance
+    for key in EXPECTED_COST_DECOMPOSITION:
+        for subkey in EXPECTED_COST_DECOMPOSITION[key]:
+            np.testing.assert_allclose(costs[key][subkey], EXPECTED_COST_DECOMPOSITION[key][subkey], rtol=1e-2)
