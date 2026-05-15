13d41e2fc5514c494a06b9507fb1cbcec916fd81
Wouter Jan Klerk <wouterjan.klerk@deltares.nl>
2026-05-15 15:50:15 +0200

    add reuse of clay layer as new top to cost computation

--- app/cost_calculator.py
+++ app/cost_calculator.py
@@ -557,7 +557,7 @@ class CostCalculator:
             real_estate_risk_cost=real_estate_risk_cost,
         )
 
-    def calc_direct_cost_ground_work(self, volumes: dict) -> DirectCostGroundWork:
+    def calc_direct_cost_ground_work(self, volumes: dict, reuse_clay_as_top: bool = False) -> DirectCostGroundWork:
         """
         Calculate the benoemde directe bouwkosten for ground work based on volumes and unit prices.
         """
@@ -570,22 +570,38 @@ class CostCalculator:
         envelop_AHN_surface = volumes['envelop_AHN_surface'] # m2
         full_design_surface = volumes['full_design_surface'] # m2
         envelop_design_surface = volumes['envelop_design_surface'] # m2
-
-        ### Combine to get costs
-        kosten_opruimen             = self.build_cost_item(full_AHN_surface, 'Q-AW010', 'm2') # opruimen terrein
-        kosten_maaien               = self.build_cost_item(full_AHN_surface, 'Q-AW020', 'm2')  # maaien terrein
-        afgraven_toplaag            = self.build_cost_item(V1b, 'Q-GV010', 'm3')  # afgraven oude grasbekleding naar depot
-        afgraven_oud_materiaal      = self.build_cost_item(V2b, 'Q-GV030', 'm3')  # afgraven oude kleilaag en zand naar depot #TODO CHECK!
-        hergebruik_oud_materiaal    = self.build_cost_item(V2b, 'Q-GV050', 'm3')  # hergebruiken oude kleilaag en zand in nieuwe kern. Als opvulling volume oude kleilaag.
-        aanvullen_kern              = self.build_cost_item(V5 + V1b, 'Q-GV090', 'm3')  # aanvullen nieuwe kern met nieuw materiaal. V2b is al aangevuld
-        profileren_dijkkern         = self.build_cost_item(envelop_design_surface, 'Q-GV100', 'm2')  # profileren van dijkkern
-        aanbrengen_nieuwe_kleilaag  = self.build_cost_item(V4, 'Q-GV080', 'm3')  # aanbrengen nieuwe kleilaag
-        profileren_nieuwe_kleilaag  = self.build_cost_item(envelop_design_surface, 'Q-GV110', 'm2')  # profileren nieuwe kleilaag
-        hergebruik_toplaag          = self.build_cost_item(V1b, 'Q-GV060', 'm3')  # hergebruiken teelaarde in nieuwe toplaag
-        aanvullen_toplaag           = self.build_cost_item(max(0, V3 - V1b), 'Q-GV070', 'm3')  # aanvullen teelaarde in nieuwe toplaag
-        profileren_nieuwe_toplaag   = self.build_cost_item(full_design_surface, 'Q-GV120', 'm2')  # profileren nieuwe graslaag en inzaaien
-        inzaaien_nieuwe_toplaag     = self.build_cost_item(full_design_surface, 'Q-AW030', 'm2')  # profileren nieuwe graslaag en inzaaien
-        
+        if not reuse_clay_as_top:
+            ### Combine to get costs
+            kosten_opruimen             = self.build_cost_item(full_AHN_surface, 'Q-AW010', 'm2') # opruimen terrein
+            kosten_maaien               = self.build_cost_item(full_AHN_surface, 'Q-AW020', 'm2')  # maaien terrein
+            afgraven_toplaag            = self.build_cost_item(V1b, 'Q-GV010', 'm3')  # afgraven oude grasbekleding naar depot
+            afgraven_oud_materiaal      = self.build_cost_item(V2b, 'Q-GV030', 'm3')  # afgraven oude kleilaag en zand naar depot #TODO CHECK!
+            hergebruik_oud_materiaal    = self.build_cost_item(V2b, 'Q-GV050', 'm3')  # hergebruiken oude kleilaag en zand in nieuwe kern. Als opvulling volume oude kleilaag.
+            aanvullen_kern              = self.build_cost_item(V5 + V1b, 'Q-GV090', 'm3')  # aanvullen nieuwe kern met nieuw materiaal. V2b is al aangevuld
+            profileren_dijkkern         = self.build_cost_item(envelop_design_surface, 'Q-GV100', 'm2')  # profileren van dijkkern
+            aanbrengen_nieuwe_kleilaag  = self.build_cost_item(V4, 'Q-GV080', 'm3')  # aanbrengen nieuwe kleilaag
+            profileren_nieuwe_kleilaag  = self.build_cost_item(envelop_design_surface, 'Q-GV110', 'm2')  # profileren nieuwe kleilaag
+            hergebruik_toplaag          = self.build_cost_item(V1b, 'Q-GV060', 'm3')  # hergebruiken teelaarde in nieuwe toplaag
+            aanvullen_toplaag           = self.build_cost_item(max(0, V3 - V1b), 'Q-GV070', 'm3')  # aanvullen teelaarde in nieuwe toplaag
+            profileren_nieuwe_toplaag   = self.build_cost_item(full_design_surface, 'Q-GV120', 'm2')  # profileren nieuwe graslaag en inzaaien
+            inzaaien_nieuwe_toplaag     = self.build_cost_item(full_design_surface, 'Q-AW030', 'm2')  # profileren nieuwe graslaag en inzaaien
+        else:
+            ### Combine to get costs
+            V_klei_in_toplaag = min(V2b, V4) # het volume klei dat hergebruikt kan worden in de nieuwe toplaag. Max is het volume van de nieuwe toplaag (V3)
+            V_klei_in_kern = max(0, V2b - V_klei_in_toplaag) # het volume klei dat hergebruikt kan worden in de nieuwe kern. Rest van V2b na hergebruik in toplaag.
+            kosten_opruimen             = self.build_cost_item(full_AHN_surface, 'Q-AW010', 'm2') # opruimen terrein
+            kosten_maaien               = self.build_cost_item(full_AHN_surface, 'Q-AW020', 'm2')  # maaien terrein
+            afgraven_toplaag            = self.build_cost_item(V1b, 'Q-GV010', 'm3')  # afgraven oude grasbekleding naar depot
+            afgraven_oud_materiaal      = self.build_cost_item(V2b, 'Q-GV030', 'm3')  # afgraven oude kleilaag en zand naar depot #TODO CHECK!
+            aanvullen_kern              = self.build_cost_item(V5 + V1b + V2b - V_klei_in_kern, 'Q-GV090', 'm3')  # aanvullen nieuwe kern met nieuw materiaal. 
+            profileren_dijkkern         = self.build_cost_item(envelop_design_surface, 'Q-GV100', 'm2')  # profileren van dijkkern
+            hergebruik_oud_materiaal    = self.build_cost_item(V_klei_in_toplaag, 'Q-GV050', 'm3')  # hergebruiken oude kleilaag in nieuwe kleilaag.
+            aanbrengen_nieuwe_kleilaag  = self.build_cost_item(V4 - V_klei_in_toplaag, 'Q-GV080', 'm3')  # aanbrengen nieuwe kleilaag V2b wordt hergebruikt
+            profileren_nieuwe_kleilaag  = self.build_cost_item(envelop_design_surface, 'Q-GV110', 'm2')  # profileren nieuwe kleilaag
+            hergebruik_toplaag          = self.build_cost_item(V1b, 'Q-GV060', 'm3')  # hergebruiken teelaarde in nieuwe toplaag
+            aanvullen_toplaag           = self.build_cost_item(max(0, V3 - V1b), 'Q-GV070', 'm3')  # aanvullen teelaarde in nieuwe toplaag
+            profileren_nieuwe_toplaag   = self.build_cost_item(full_design_surface, 'Q-GV120', 'm2')  # profileren nieuwe graslaag en inzaaien
+            inzaaien_nieuwe_toplaag     = self.build_cost_item(full_design_surface, 'Q-AW030', 'm2')  # profileren nieuwe graslaag en inzaaien            
 
         return DirectCostGroundWork(
             kosten_opruimen=kosten_opruimen,
